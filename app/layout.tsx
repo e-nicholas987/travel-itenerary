3abe3a265e541babdf4728c960f159e290166421
e.nicholas987 <i.emeka74@yahoo.com>
2025-11-25 12:13:16 +0100

    refactor(layout): update padding variables for responsive design

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,13 +31,11 @@ export default function RootLayout({
           <header className="h-(--topbar-height) fixed bg-white top-0 z-10 inset-x-0">
             <Topbar />
           </header>
-          <div className="layout-shell max-w-400 mx-auto flex min-h-svh bg-neutral-300 flex-col lg:flex-row gap-4 lg:gap-8 2xl:gap-16 p-4 md:p-5 2xl:p-10">
-            <aside className="hidden lg:block lg:w-75 h-fit max-h-[calc(100vh-12.5rem)] overflow-y-auto shrink-0 lg:sticky top-layout-offset left-0">
+          <div className="layout-shell max-w-400 mx-auto flex min-h-svh bg-neutral-300 flex-col lg:flex-row gap-4 lg:gap-8 2xl:gap-16 p-(--layout-padding)">
+            <aside className="hidden lg:block lg:w-75 max-h-[calc(100vh-var(--layout-offset)-var(--layout-padding))] overflow-y-auto shrink-0 lg:sticky top-layout-offset left-0">
               <Sidebar />
             </aside>
-            <main className="flex flex-1 bg-white rounded-sm mt-4 lg:mt-0">
-              {children}
-            </main>
+            <main className="flex flex-1 bg-white rounded-sm">{children}</main>
           </div>
         </QueryProvider>
       </body>
